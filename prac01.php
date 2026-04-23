c39189a7686615acf315b8427241350943820bf2
Farnaz-01 <farnazfaria12@gmail.com>
2026-04-23 14:44:15 +0600

    Updating Jarin and Rafa

---
+++ prac01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /* echo "I am lrarning PHP<br>";
+    echo "My name is RAFA <br>";
+
+    $x=10;
+    $y=5;
+    echo $x+$y."<br>"; 
+    var_dump($x); echo "<br>";
+    var_dump(1>5);
+    $my_var=(false)&&(true);
+    echo var_dump($my_var);
+    echo "<br>";
+    $car = array("brand"=>"Ford", "brand"=>"RaFa","model"=>"Mustang", "year"=>1964);
+    echo $car["brand"];*/
+
+    ECHO  8+5;
+    echo "<br>";
+    Echo 17-2;
+
+    ?>
+</body>
+</html>
